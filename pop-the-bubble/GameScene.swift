43a868be670169a3115df39755ae677f80f46d07
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-10-11 11:57:27 -0700

    Started the bubble pop game and allowed users to tap on boxes and it disappears

--- pop-the-bubble/GameScene.swift
+++ pop-the-bubble/GameScene.swift
@@ -8,82 +8,58 @@
 
 import SpriteKit
 import GameplayKit
+import Foundation
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+    override func didMove(to view: SKView){
+        let wait = SKAction.wait(forDuration: 1.5)
+        let makeABox = SKAction.run {
+            self.makeBox()
         }
+        let seq = SKAction.sequence([wait, makeABox])
+        let req = SKAction.repeatForever(seq)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        run (req)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+    }
+    
+    func makeBox() {
+        
+        // define a size for a box
+        let boxSize = CGSize(width: 40, height: 40)
+        // make a red box of size
+        let box = SKSpriteNode(texture: nil, color: .red, size: boxSize)
+        box.name = "box"
+        
+        // Get a random possition within the width of the scene
+        let x = CGFloat(randomize(number: Int(size.width - 40)) + 20)
+        
+        // position the box
+        box.position.x = x
+        box.position.y = 0
+        
+        // Add the box
+        addChild(box)
+        
+        // craete an action to move the move up teh screen
+        let moveUp = SKAction.moveTo(y: size.height, duration: 4)
+        let seq = SKAction.sequence([moveUp, .removeFromParent()])
+        box.run(seq)
+    }
+    
+    // function that returns a random int from 0 to n-1
+    func randomize(number: Int) -> Int{
+        return Int(arc4random()) % number
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        if let touch = touches.first {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            let location = touch.location(in: self)
+            let node = atPoint(location)
+            if node.name == "box" {
+                node.removeFromParent()
+            }
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
--- pop-the-bubble/GameViewController.swift
+++ pop-the-bubble/GameViewController.swift
@@ -15,23 +15,24 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        // set up our view to present our game
+        guard let view = self.view as? SKView else {return}
+        
+        // set up our game scene
+        let gameScene = GameScene(size: self.view.frame.size)
+        gameScene.scaleMode = .aspectFit
+        
+        // present our game scene
+        view.presentScene(gameScene)
+        
+        // display debug info
+        view.showsFPS = true
+        view.showsNodeCount = true
+        
+        
+        
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
